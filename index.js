c73c9e87819576093347234c0a929d157dc26aff
mrobertsjr <70207997+mrobertsjr@users.noreply.github.com>
2020-12-30 07:14:27 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+var changeName = document.querySelector('ul li:nth-child(3)');
+changeName.innerHTML = 'Mike';
+document.querySelector('li a').style.color = 'green';
+document.querySelector('button').style.backgroundColor = 'yellow';
+document.querySelector('h1').classList.toggle('huge');
